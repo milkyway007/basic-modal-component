a74d69cca1cb71815d67a9db45b004f9f39d0129
Liidia Laada <liidia.laada@gmail.com>
2022-05-16 11:52:45 +0300

    Named slots, listening to slot content changes, closing the modal, dispatching and configuring custom events.

--- modal.js
+++ modal.js
@@ -2,6 +2,7 @@ class Modal extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
+        this.isOpen = false;
         this.shadowRoot.innerHTML = `
             <style>
                 #backdrop {
@@ -22,9 +23,13 @@ class Modal extends HTMLElement {
                     pointer-events: all;
                 }
 
+                :host([opened]) #modal {
+                    top: 15vh;
+                }
+
                 #modal {
                     position: fixed;
-                    top: 15vh;
+                    top: 10vh;
                     left: 25%;
                     width: 50%;
                     z-index: 100;
@@ -36,14 +41,17 @@ class Modal extends HTMLElement {
                     justify-content: space-between;
                     opacity: 0;
                     pointer-events: none;
+                    transition: all 0.3s ease-out;
                 }
 
                 header {
                     padding: 1rem;
+                    border-bottom: 1px solid #ccc;
                 }
 
-                header h1{
+                ::slotted(h1) {
                     font-size: 1.25rem;
+                    margin: 0;
                 }
 
                 #main {
@@ -64,17 +72,70 @@ class Modal extends HTMLElement {
             <div id="backdrop"></div>
             <div id="modal">
                 <header>
-                    <h1>Please confirm</h1>
+                    <slot name="title">Please Confirm Payment</slot>
                 </header>
                 <section id="main">
                     <slot></slot>
                 </section>
                 <section id="actions">
-                    <button>Cancel</button>
-                    <button>Confirm</button>
+                    <button id="cancel-btn">Cancel</button>
+                    <button id="confirm-btn">Confirm</button>
                 </section>
             </div>
         `;
+
+        const slots = this.shadowRoot.querySelectorAll('slot');
+        slots[1].addEventListener('slotchange', event => {
+            console.dir(slots[1].assignedNodes());
+        });
+
+        const backdrop = this.shadowRoot.querySelector('#backdrop');
+
+        const cancelButton = this.shadowRoot.querySelector('#cancel-btn');
+        const confirmButton = this.shadowRoot.querySelector('#confirm-btn');
+
+        backdrop.addEventListener('click', this._cancel.bind(this));
+        cancelButton.addEventListener('click', this._cancel.bind(this));
+        confirmButton.addEventListener('click', this._confirm.bind(this));
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === 'opened') {
+            if (this.hasAttribute('opened')) {
+                this.isOpen = true;
+            }
+            else {
+                this.isOpen = false;
+            }
+        }
+    }
+
+    static get observedAttributes() {
+        return ['opened'];
+    }
+
+    open() {
+        this.setAttribute('opened', '');
+        this.isOpen = true;
+    }
+
+    hide () {
+        if (this.hasAttribute('opened')) {
+            this.removeAttribute('opened');
+        }
+        this.isOpen = false;
+    }
+
+    _cancel(event) {
+        this.hide();
+        const cancelEvent = new Event('cancel', {bubbles: true, composed: true});
+        event.target.dispatchEvent(cancelEvent);
+    }
+
+    _confirm() {
+        this.hide();
+        const cancelEvent = new Event('confirm');
+        this.dispatchEvent(cancelEvent);
     }
 }
 
